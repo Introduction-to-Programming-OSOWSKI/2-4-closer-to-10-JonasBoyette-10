bc5ea35bd0b9e1f4304e130eb4d0a6009d2c9d8b
JonasBoyette-10 <81956@mystma.org>
2022-09-21 14:47:12 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#WRITE YOUR CODE IN THIS FILE
+def Close10(x, y):
+    if abs(10-x) < abs(10-y):
+        return x
+    elif abs(10-x) > abs(10-y):
+        return y
+    else:
+        return 0
+
+print(Close10(9,15))
+
